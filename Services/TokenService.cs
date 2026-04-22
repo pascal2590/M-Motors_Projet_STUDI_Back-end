90e1eb3d696c0fb400ccab6d31efa24e9f3e0208
pascal2590 <155544942+pascal2590@users.noreply.github.com>
2026-04-22 12:45:28 +0200

    Correction de bugs d'affichage du Tableau de bord

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -13,12 +13,13 @@ namespace m_motors_API.Services
         {
             _config = config;
         }
-
         public string GenerateToken(
             string email,
             string role,
             string typeUtilisateur,
-            int userId)
+            int userId,
+            string? prenom,
+            string? nom)
         {
             var secret = _config["Jwt:Key"];
             var issuer = _config["Jwt:Issuer"];
@@ -30,25 +31,28 @@ namespace m_motors_API.Services
                 key,
                 SecurityAlgorithms.HmacSha256);
 
+
+            // CLAIMS JWT
             var claims = new[]
             {
+                // identité
                 new Claim(ClaimTypes.Name, email),
                 new Claim(ClaimTypes.Role, role),
                 new Claim("Type", typeUtilisateur),
 
-                // IMPORTANT
-                new Claim(ClaimTypes.NameIdentifier, userId.ToString())
-            };
+                // id utilisateur
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
 
+                // identité utilisateur (client uniquement)
+                new Claim(ClaimTypes.GivenName, prenom ?? ""),
+                new Claim(ClaimTypes.Surname, nom ?? "")
+            };
 
             var token = new JwtSecurityToken(
                 issuer: issuer,
                 audience: issuer,
-
                 claims: claims,
-
                 expires: DateTime.UtcNow.AddMinutes(30),
-
                 signingCredentials: creds
             );
 
